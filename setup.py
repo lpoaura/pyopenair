4ae71e669c88f8b7f879de01c575635fd9f23c50
lpofredc <frederic.cloitre@lpo.fr>
2020-11-02 22:57:18 +0100

    Update to v0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyopenair",
-    version="0.1.0-dev",
+    version="0.1.0",
     author="lpofredc",
     author_email="frederic.cloitre@lpo.fr",
     packages=["pyopenair"],
