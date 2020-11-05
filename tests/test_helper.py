5862cdabba946e52db9130578ccbf4ee23a216b8
lpofredc <frederic.cloitre@lpo.fr>
2020-11-05 16:18:11 +0100

    WIP tests

---
+++ tests/test_helper.py
@@ -0,0 +1,12 @@
+from pyopenair.helper import (
+    stringify_coords,
+    altitude_formatter,
+    comment_formatter,
+    fields_formatter,
+    generate_coords,
+    generate_openair_coord,
+)
+
+
+def test_stringify_coords():
+    pass
