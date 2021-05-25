a586fb3c52a33347de15e24a8c4f5a699ccddf1a
lpofredc <frederic.cloitre@lpo.fr>
2021-05-25 11:42:05 +0200

    docs: update Docs version

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = "2020, lpofredc"
 author = "lpofredc"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "1.1.0"
 
 # -- General configuration ---------------------------------------------------
 
